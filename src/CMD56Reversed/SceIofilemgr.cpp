bad80da09558c9aaf5afb8f046735ee38a9818df
motoharu-gosuto <motoharu.gosuto@gmail.com>
2017-04-23 23:11:35 +0400

    simplify mount_switch_case_1

--- src/CMD56Reversed/SceIofilemgr.cpp
+++ src/CMD56Reversed/SceIofilemgr.cpp
@@ -15,19 +15,6 @@ typedef struct vfs_block_dev_info //size is 0x14
   uint32_t vshMountId; //must be same as in mount_point_info
 }vfs_block_dev_info;
 
-typedef struct vfs_mount_point_info_base
-{
-  char* unixMount;
-  char* unk_4; //zero
-  uint32_t devMajor;
-  uint32_t devMinor;
-
-  char* filesystem;
-  uint32_t unk_14; //zero
-  vfs_block_dev_info* blockDev1;
-  uint32_t unk_1C; //zero
-} vfs_mount_point_info_base;
-
 //probably vfsops
 typedef struct node_ops1 // size is 0x34 (13 pointers)
 {
@@ -80,6 +67,19 @@ typedef struct node_ops2 // size is 0x74 (29 pointers)
   int (*func29)(void* ctx); // not implemented by all
 } node_ops2;
 
+typedef struct vfs_mount_point_info_base
+{
+  char* unixMount;
+  char* unk_4; //zero
+  uint32_t devMajor;
+  uint32_t devMinor;
+
+  char* filesystem;
+  uint32_t unk_14; //zero
+  vfs_block_dev_info* blockDev1;
+  node_ops2* unk_1C; //zero
+} vfs_mount_point_info_base;
+
 typedef struct vfs_add_data
 {
     node_ops1* funcs1;
@@ -127,7 +127,7 @@ typedef struct vfs_node
    uint32_t unk_C0;
    uint32_t unk_C4; //mountInfo->unk_14
    uint32_t unk_C8;
-   uint32_t unk_CC;   
+   void* unk_CC;   
    
    uint32_t unk_D0; //is this field present ? most likely it is not (need to find some D0 constants)
 
@@ -160,17 +160,17 @@ int SceIofilemgrForDriver_dc2d8bce(vfs_node* a0)
    return 0;
 }
 
-int sub_BEC578(int a0, int a1)
+int sub_BEC578(vfs_node* a0, vfs_node* a1)
 {
    return 0;
 }
 
-int sub_BEC620(int a0)
+int sub_BEC620(vfs_node* a0)
 {
    return 0;
 }
 
-int sub_BEC010(int a0)
+int sub_BEC010(vfs_node* a0)
 {
    return 0;
 }
@@ -361,21 +361,19 @@ int loc_BE7252(vfs_node* n0, char* filesystem, int errorCode, vfs_node* unk2, vo
    return loc_BE6C96(filesystem, errorCode, unk3, var_D8, cookie);
 }
 
-int loc_BE76C8(vfs_node* n0, int r7, char* filesystem, int errorCode, vfs_node* unk2, void* unk3, void* var_D8, int cookie)
+int loc_BE76C8(vfs_node* n0, vfs_node* r7, char* filesystem, int errorCode, vfs_node* unk2, void* unk3, void* var_D8, int cookie)
 {
-   /*
-   SceIofilemgrForDriver_6b3ca9f7(n0[0x4C]); //lock print
+   SceIofilemgrForDriver_6b3ca9f7(n0->node); //4C mutex lock print
 
-   n0[0x4C][0x60] = n0[0x4C][0x60] - 1; //counter ?
+   n0->node->unk_60--; //counter
    
-   sub_BEC578(n0[0x4C], r7);
+   sub_BEC578(n0->node, r7);
    
-   SceIofilemgrForDriver_dc2d8bce(n0[0x4C]); //unlock print
+   SceIofilemgrForDriver_dc2d8bce(n0->node); //4C mutex  unlock print
 
    sub_BEC620(r7);
    
    sub_BEC010(r7);
-   */
    
    return loc_BE7252(n0, filesystem, errorCode, unk2, unk3, var_D8, cookie);
 }
@@ -504,120 +502,91 @@ int mount_switch_case_1(vfs_mount_point_info_base *mountInfo, vfs_add_data* addD
          return loc_BE7252(n0, mountInfo->filesystem, result3, unk2, unk3, var_D8, cookie);
    }
     
-    //loc_BE7216:
+   //loc_BE7216:
     
-    uint32_t var_28;
+   uint32_t var_28;
 
-    void* r2 = (&var_28) + ((unk1 - 1) << 3);
+   void* r2 = (&var_28) + ((unk1 - 1) << 3);
     
-    uint32_t node = r2[-0x84];   // D0 - 84 = 4C - can be part of vfs_node ?
-    uint32_t var_B8 = r2[-0x80]; // D0 - 80 = 50 - can be part of vfs_node ?
+   uint32_t node = r2[-0x84];   // D0 - 84 = 4C - can be part of vfs_node ?
+   uint32_t var_B8 = r2[-0x80]; // D0 - 80 = 50 - can be part of vfs_node ?
 
-    void* var_B4 = unk3;
+   void* var_B4 = unk3;
     
-    int result4 = sub_BE62E8(mountInfo->unixMount, var_D8, unk0, &unk1, 0x01);
-    if(result4 < 0)
-        return loc_BE7252(n0, mountInfo->filesystem, result4, unk2, unk3, var_D8, cookie);
+   int result4 = sub_BE62E8(mountInfo->unixMount, var_D8, unk0, &unk1, 0x01);
+   if(result4 < 0)
+      return loc_BE7252(n0, mountInfo->filesystem, result4, unk2, unk3, var_D8, cookie);
         
-    int r3 = unk1;
-    if(r3 == 0)
-        return loc_BE7252(n0, mountInfo->filesystem, result4, unk2, unk3, var_D8, cookie);
+   int r3 = unk1;
+   if(r3 == 0)
+      return loc_BE7252(n0, mountInfo->filesystem, result4, unk2, unk3, var_D8, cookie);
         
 //loc_BE74F4:        
-    vfs_node* bnode = proc_get_arg0_for_sceVfsGetNewNode_BEBAC0();
-    if(bnode == 0)
-        return loc_BE7252(n0, mountInfo->filesystem, 0x8001000C, unk2, unk3, var_D8, cookie);    
+   vfs_node* bnode = proc_get_arg0_for_sceVfsGetNewNode_BEBAC0();
+   if(bnode == 0)
+      return loc_BE7252(n0, mountInfo->filesystem, 0x8001000C, unk2, unk3, var_D8, cookie);    
 
-    vfs_add_data* r6 = addData;
-
-    proc_init_SceVfsMnt_BEBB84(bnode, n0, get_SceIoVfsHeap_id(), addData); // important initialization function
+   proc_init_SceVfsMnt_BEBB84(bnode, n0, get_SceIoVfsHeap_id(), addData); // important initialization function
   
-    bnode->blockDev = mountInfo->blockDev1;
+   bnode->blockDev = mountInfo->blockDev1;
     
-    strncpy(bnode->unk_80, (char*)var_D8, 0x40); //it should be string since it is terminated with 0 explicitly
+   strncpy(bnode->unk_80, (char*)var_D8, 0x40); //it should be string since it is terminated with 0 explicitly
 
-    bnode->unk_80[0x3F] = (char)0; //terminate with 0
+   bnode->unk_80[0x3F] = (char)0; //terminate with 0
 
-    bnode->unk_C4 = mountInfo->unk_14;
+   bnode->unk_C4 = mountInfo->unk_14;
 
-    bnode->node = (vfs_node*)mountInfo->devMajor; //0x4C 
-    bnode->prev_node = (vfs_node*)(mountInfo->devMinor & 0xFFFFF); //0x50 - take first 0x14 bits
+   bnode->node = (vfs_node*)mountInfo->devMajor; //0x4C 
+   bnode->prev_node = (vfs_node*)(mountInfo->devMinor & 0xFFFFF); //0x50 - take first 0x14 bits
 
-    bnode->dev_info = (void*)0x101; //0x48    
+   bnode->dev_info = (void*)0x101; //0x48    
     
-    SceIofilemgrForDriver_6b3ca9f7(n0->node); //0x4C mutex lock print
+   SceIofilemgrForDriver_6b3ca9f7(n0->node); //0x4C mutex lock print
    
-    n0->node->unk_60++; //counter
+   n0->node->unk_60++; //counter
     
-    sub_BEC56C(n0->node, bnode);
+   sub_BEC56C(n0->node, bnode);
     
-    SceIofilemgrForDriver_dc2d8bce(n0->node); //0x4C mutex unlock print
+   SceIofilemgrForDriver_dc2d8bce(n0->node); //0x4C mutex unlock print
     
-/*
-    int r2 = n0;
-    int r3 = bnode[0x4C];
-    int r2 = r2[0x4C];
+   bnode->node = bnode->node | (((short)n0->node[0x4E]) << 16);
     
-    short r2 = r2[0x4E];
-    
-    r3 = r3 | (r2 << 16)
-    
-    bnode[0x4C] = r3;
-    
-    uint32_t r11 = mountInfo->unk_1C;
-    if(r11 == 0)
-    {
-        r11 = r6[0x14]; //vfs_add_data* ? node_ops2* ?
-    }
+   vfs_add_data* r6 = addData;
+
+   node_ops2* ops2 = mountInfo->unk_1C == 0 ? addData->funcs2 : mountInfo->unk_1C;
+
+   vfs_node* vnode;
 
 //loc_BE7576:    
-    node_ops2* r1 = r11;
-    vfs_node* r0 = bnode;
-    int r2 = 0;
-    vfs_node** r3 = &vnode;
-    int r0 = SceIofilemgrForDriver_sceVfsGetNewNode_d60b5c63(r0, r1, r2, r3);
-    int r9 = r0;
-    if(r9 < 0)
-        return loc_BE76C8(n0, bnode, mountInfo->filesystem, r9, unk2, unk3, var_D8, var_2C, r4);
+   int result5 = SceIofilemgrForDriver_sceVfsGetNewNode_d60b5c63(bnode, ops2, 0, &vnode);
+   if(result5 < 0)
+      return loc_BE76C8(n0, bnode, mountInfo->filesystem, result5, unk2, unk3, var_D8, cookie);
     
-    int r3 = bnode[0x50];
-    int r0 = r3 << 0x0E
-    if(r0 < 0)
-    {
-       short r2 = bnode[0x4E];
-       int r2 = r2 & 0xF00;
-       if(r2 != 0x200)
-       {
-         r3 = r3 & (~0x20000);
-         bnode[0x50] = r3;
-       }
-    }
+   if(((uint32_t)bnode->prev_node << 0x0E) < 0) //50
+   {
+      if(((short)bnode[0x4E] & 0xF00) != 0x200)
+      {
+         bnode->prev_node = (uint32_t)bnode->prev_node & (~0x20000); //50
+      }
+   }
     
 //loc_BE75A4:    
-    int r2 = n0;
-    int r3 = r2[0x4C];
-    int r1 = r3[0x50];
-    int r1 = r1 << 0x0E;
     
-    if(r1 < 0)
-    {
-        short r1 = r3[0x4E];
-        int r1 = r1 & 0xF00;
-        if(r1 == 0x200)
-        {
-            int r3 = r3[0xCC];
-            int r0 = bnode[0x50];
-            int r6 = bnode[0xCC];
-            int r1 = r3[0x08];
-            int r3 = r3[0x0C];
-            int r0 = r0 | 0x20000;
-            
-            r6[0x08] = r1;
-            r6[0x0C] = r3;
-            bnode[0x50] = r0;
-        }
-    }
+   int r3 = n0->node; //4C
+
+   if((((uint32_t)n0->node->prev_node) << 0x0E) < 0) //4C 50
+   {
+      if((((short)n0->node[0x4E]) & 0xF00) == 0x200)
+      {
+         //this code proves that 'bnode' and 'node' have same type
+
+         bnode->unk_CC[0x08] = n0->node->unk_CC[0x08];
+         bnode->unk_CC[0x0C] = n0->node->unk_CC[0x0C];
+         bnode->prev_node = bnode->prev_node | 0x20000; //50
+      }
+   }
     
+/*
 //loc_BE75D4:    
     int r0 = r2[0x70];
     int r1 = 0x1002;
