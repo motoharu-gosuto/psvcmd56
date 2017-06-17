efaf278a64403f7d49ece0bbd33bf5492ffd4cc6
motoharu-gosuto <motoharu.gosuto@gmail.com>
2017-06-18 03:47:56 +0400

    raw implementation of rif dec

--- src/CMD56Reversed/SceNpDrm.cpp
+++ src/CMD56Reversed/SceNpDrm.cpp
@@ -1,148 +1,240 @@
+#include <string>
+#include <stdint.h>
 
 
 void loc_1D37F94()
 {
-   /*
-   LDR             R4, [SP,#0x298+var_260]
-   MOV             R4, 0x1C8D680 ; 01D37F96 : external reference: 01C8D680 - Secondary Table of act.dat
-   STR             R4, [SP,#0x298+var_260] ; = 1C8D680
-   LDMIA           R4!, {R0-R3} ; get decrypted RIF key
-   STR.W           R0, [R11] ; store decrypted key
-   STR.W           R1, [R11,#4]
-   STR.W           R2, [R11,#8]
-   STR.W           R3, [R11,#0xC]
-   B               loc_1D37F58
-   */
-}
+   int r4 = 0x1C8D680;
+   var_260 = r4;
 
-void loc_1D37FFA()
-{
-   /*
-   LDR             R5, [SP,#0x298+var_25C]
-   STR             R7, [SP,#0x298+arg_0] ; arg_0 = 0 - optimization
-   LDR             R4, [SP,#0x298+unk0] ; 1C8D680
-   MOV             R5, 0x1C8C030 ; 01D38000 : external reference: 01C8C030
-   LDMIA           R5!, {R0-R3} ; get static key 3
-   ADDS            R4, #0x40 ; 1C8D680 + 0x40
-   STMIA           R4!, {R0-R3} ; store static key 3
-   STR             R5, [SP,#0x298+var_25C] ; 1C8C040
-   LDMIA.W         R5, {R0-R3} ; get static key 4
-   STMIA.W         R4, {R0-R3} ; store static key 4
-   SUB.W           R2, R4, #0x10 ; keypair
-   LDR             R0, [SP,#0x298+unk0] ; buffer - 1C8D680
-   MOVS            R1, #0x10 ; num1
-   MOVS            R3, #0x90 ; num2
-   BLX             SceNpDrm.SceSblAuthMgrForDriver._imp_sceSblAuthMgrDecBindData_41daea12
-   */
+   int r0 = r4[0x00];
+   int r1 = r4[0x04];
+   int r2 = r4[0x08];
+   int r3 = r4[0x0C];
 
-   if(r0 < 0)
-   {
-      //loc_1D3802A
-   }
-   else
-   {
-      //goto loc_1D37F94
-   }
-}
-
-void loc_1D37F2E()
-{
-   /*
-   MOVW            R0, #0xD680
-   MOVS            R3, #0
-   MOVT.W          R0, #0x1C8 ; 01D37F34 : external reference: 01C8D680 - Secondary Table of act.dat
-   STR             R3, [SP,#0x298+arg_0] ; arg_0 = 0
-   ADD.W           R2, R0, #0x40 ; unk2 - 01C8D680 + 0x40 - primary key pair
-   MOVS            R1, #0x10 ; num1
-   MOVS            R3, #0x90 ; num2
-   BLX             SceNpDrm.SceSblAuthMgrForDriver._imp_sceSblAuthMgrDecBindData_41daea12
-   */
-
-   if(r0 < 0)
-   {
-      //goto loc_1D3802A
-   }
-   else
-   {
-      //goto loc_1D37F94
-   }
+   r11[0x00] = r0;
+   r11[0x04] = r1;
+   r11[0x08] = r2;
+   r11[0x0C] = r3;
 }
 
 void get_rif_key_1D37B48_part()
 {
-   /*
-   LDR             R5, [SP,#0x298+unk0]
-   MOV.W           R1, #0x100 ; size
-   MOV             R5, 0x1C8D680 ; 01D37EF0 : external reference: 01C8D680 - looks like Secondary Table of act.dat
-   MOV             R0, R5  ; ptr
-   STR             R5, [SP,#0x298+unk0] ; = 1C8D680
-   BL              zeromem_1D3AB0C
-   ADD             R3, SP, #0x298+rif_data+0xA0
-   LDMIA           R3, {R0-R3} ; get 0x10 bytes rif data 0xA0
-   STMIA.W         R5, {R0-R3} ; store 0x10 bytes
-   MOV             R0, R5  ; 1C8D680
-   ADDS            R0, #0x60 ; destination
-   ADD             R1, SP, #0x298+rif_data ; source
-   MOVS            R2, #0x70 ; num
-   BLX             SceNpDrm.SceSysclibForDriver._imp_memcpy_40c88316 ; copy 0x70 bytes of rif data
-   */
+   int r1 = 0x100;
+   int r5 = 0x1C8D680
+   int r0 = r5;
+   unk0 = r5;
+
+   memset(r0, 0, r1);
+   
+   int r3 = rif_data+0xA0;
+
+   int r0 = r3[0x00]
+   int r1 = r3[0x04]
+   int r2 = r3[0x08]
+   int r3 = r3[0x0C]
+
+   r5[0x00] = r0;
+   r5[0x04] = r1;
+   r5[0x08] = r2;
+   r5[0x0C] = r3;
+
+   int r0 = 1C8D680;
+   int r0 = r0 + 0x60;
+   int r1 = rif_data;
+   int r2 = 0x70;
+
+   memcpy(r0, r1, r2);
 
    if(r7 == 0)
    {
-      /*
-      loc_1D37FB4             ;
-      LDR             R4, [SP,#0x298+value] ; get byte of rif_lic_flags
-      */
+      int r4 = value; //byte of rif_lic_flags
 
       if(r4 == 0xD)
       {
-         //goto loc_1D37FFA
+         arg_0 = R7;
+         int r4 = unk0;
+         int r5 = 0x1C8C030;
+
+         int r0 = r5[0x00];
+         int r1 = r5[0x04];
+         int r2 = r5[0x08];
+         int r3 = r5[0x0C];
+         r5 = r5 + 0x10;
+
+         int r4 = r4 + 0x40;
+
+         r4[0x00] = r0;
+         r4[0x04] = r1;
+         r4[0x08] = r2;
+         r4[0x0C] = r3;
+         r4 = r4 + 0x10;
+         
+         var_25C = r5;
+
+         int r0 = r5[0x00];
+         int r1 = r5[0x04];
+         int r2 = r5[0x08];
+         int r3 = r5[0x0C];
+
+         r4[0x00] = r0;
+         r4[0x04] = r1;
+         r4[0x08] = r2;
+         r4[0x0C] = r3;
+
+         r2 = r4 - 0x10;
+         r0 = unk0;
+         r1 = 0x10;
+         r3 = 0x90;
+
+         sceSblAuthMgrDecBindData_41daea12(r0,r1,r2,r3,arg_0);
+
+         if(r0 < 0)
+         {
+            //loc_1D3802A
+         }
+         else
+         {
+            //goto loc_1D37F94
+         }
       }
       else
       {
-         /*
-         LDR             R4, [SP,#0x298+unk0] ; 1C8D680
-         SUB.W           R5, R4, #0x870 ; 1C8CE10 - Primary Key Table
-         ADDS            R4, #0x40
-         LDMIA           R5!, {R0-R3} ; get primary key 0x10 bytes
-         STMIA           R4!, {R0-R3} ; store primary key
-         LDMIA.W         R5, {R0-R3} ; get primary key 0x10 bytes
-         STMIA.W         R4, {R0-R3} ; store primary
-         */
+         int r4 = unk0;
+         int r5 = r4 - 0x870;
+         int r4 = r4 + 0x40;
 
-         //goto loc_1D37F2E
+         int r0 = r5[0x00];
+         int r1 = r5[0x04];
+         int r2 = r5[0x08];
+         int r3 = r5[0x0C];
+         r5 = r5 + 0x10;
+
+         r4[0x00] = r0;
+         r4[0x04] = r1;
+         r4[0x08] = r2;
+         r4[0x0C] = r3;
+         r4 = r4 + 0x10;
+
+         int r0 = r5[0x00];
+         int r1 = r5[0x04];
+         int r2 = r5[0x08];
+         int r3 = r5[0x0C];
+
+         r4[0x00] = r0;
+         r4[0x04] = r1;
+         r4[0x08] = r2;
+         r4[0x0C] = r3;
+
+         int r0 = 01C8D680;
+         int r3 = 0;
+         arg_0 = r3;
+
+         int r2 = r0 + 0x40;
+         r1 = 0x10;
+         r3 = 0x90;
+
+         sceSblAuthMgrDecBindData_41daea12(r0,r1,r2,r3,arg_0);
+
+         if(r0 < 0)
+         {
+            //goto loc_1D3802A
+         }
+         else
+         {
+            //goto loc_1D37F94
+         }
       }
    }
    else
    {
-      /*
-      ADD             R7, SP, #0x298+cmd56_handshake
-      LDR             R5, [SP,#0x298+unk0] ; 1C8D680
-      LDMIA           R7!, {R0-R3} ; get 0x10 cmd56 bytes
-      ADDS            R5, #0x40
-      STMIA           R5!, {R0-R3}
-      LDMIA.W         R7, {R0-R3} ; get 0x10 cmd56 bytes
-      STMIA.W         R5, {R0-R3}
-      */
+      int r7 = cmd56_handshake;
+      int r5 = unk0;
+
+      int r0 = r7[0x00];
+      int r1 = r7[0x04];
+      int r2 = r7[0x08];
+      int r3 = r7[0x0C];
+      r7 = r7 + 0x10;
+
+      int r5 = r5 + 0x40;
+
+      r5[0x00] = r0;
+      r5[0x04] = r1;
+      r5[0x08] = r2;
+      r5[0x0C] = r3;
+      r5 = r5 + 0x10;
+
+      int r0 = r7[0x00];
+      int r1 = r7[0x04];
+      int r2 = r7[0x08];
+      int r3 = r7[0x0C];
+
+      r5[0x00] = r0;
+      r5[0x04] = r1;
+      r5[0x08] = r2;
+      r5[0x0C] = r3;
 
       if(r4 == 0)
       {
-         //goto loc_1D37F2E  
+         int r0 = 01C8D680;
+         int r3 = 0;
+         arg_0 = r3;
+
+         int r2 = r0 + 0x40;
+         r1 = 0x10;
+         r3 = 0x90;
+
+         sceSblAuthMgrDecBindData_41daea12(r0,r1,r2,r3,arg_0);
+
+         if(r0 < 0)
+         {
+            //goto loc_1D3802A
+         }
+         else
+         {
+            //goto loc_1D37F94
+         }
       }
       else
       { 
-         /*
-         LDR             R4, [SP,#0x298+unk0] ; 1C8D680
-         LDR             R5, [SP,#0x298+unk0] ; 1C8D680
-         ADDS            R4, #0xB0 ; RIF Key offset in copied data
-         LDMIA.W         R4, {R0-R3} ; 1C8D680 + 0xB0 - get 0x10 bytes
-         STMIA.W         R5, {R0-R3} ; store 0x10 bytes
-         MOV             R0, R4  ; ptr
-         MOVS            R1, #0x10 ; size
-         BL              zeromem_1D3AB0C ; erase RIF Key
-         */
+         int r4 = unk0;
+         int r5 = unk0;
+
+         int r4 = r4 + 0xB0;
+
+         int r0 = r4[0x00];
+         int r1 = r4[0x04];
+         int r2 = r4[0x08];
+         int r3 = r4[0x0C];
+
+         r5[0x00] = r0;
+         r5[0x04] = r1;
+         r5[0x08] = r2;
+         r5[0x0C] = r3;
+
+         int r0 = r4;
+         int r1 = 0x10;
+         memset(r0, 0, r1);
          
-         //goto loc_1D37F2E
+         int r0 = 01C8D680;
+         int r3 = 0;
+         arg_0 = r3;
+
+         int r2 = r0 + 0x40;
+         r1 = 0x10;
+         r3 = 0x90;
+
+         sceSblAuthMgrDecBindData_41daea12(r0,r1,r2,r3,arg_0);
+
+         if(r0 < 0)
+         {
+            //goto loc_1D3802A
+         }
+         else
+         {
+            //goto loc_1D37F94
+         }
       }
    }
 }
