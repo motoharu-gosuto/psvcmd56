282c07f229c8c742bf6aa4957b8c3326783e7878
motoharu-gosuto <motoharu.gosuto@gmail.com>
2017-06-18 04:27:56 +0400

    finalize rif key dec

--- src/CMD56Reversed/SceNpDrm.cpp
+++ src/CMD56Reversed/SceNpDrm.cpp
@@ -20,68 +20,64 @@ typedef struct dec_buffer
 
 //static_keys 0x1C8C010
 
-void get_rif_key_1D37B48_part(const char* act_data, const char* static_keys, const char* rif_data, const char* cmd56_handshake, char* klicensee)
+int dec_rif_key(dec_buffer* decbuf, char* klicensee)
+{
+   int r0 = sceSblAuthMgrDecBindData_41daea12(decbuf->rif_key, 0x10, decbuf->primary_keys, 0x90, 0);
+
+   if(r0 < 0)
+      memset(klicensee, 0, 0x10);
+   else
+      memcpy(klicensee, decbuf->rif_key, 0x10);
+
+   return 0;
+}
+
+void get_rif_key_1D37B48_part(const char* act_data, const char* static_keys, const char* rif_data, const char* cmd56_handshake, int lic_flag, int lic_derived_flag, int onfly_bit11, char* klicensee)
 {
    dec_buffer decbuf;
 
    memset(&decbuf, 0, 0x100);
    
-   memcpy(decbuf.rif_key, rif_data + 0xA0, 0x10);
-
    memcpy(decbuf.rif_data, rif_data, 0x70);
 
-   if(r7 == 0)
+   if(onfly_bit11 == 0)
    {
-      int r4 = value; //byte of rif_lic_flags
-
-      if(r4 == 0xD)
+      if(lic_flag == 0xD)
       {
          memcpy(decbuf.primary_keys, static_keys + 0x20, 0x20); //keys 3 and 4
 
-         int r0 = sceSblAuthMgrDecBindData_41daea12(decbuf.rif_key, 0x10, decbuf.primary_keys, 0x90, 0);
+         memcpy(decbuf.rif_key, rif_data + 0xA0, 0x10);
 
-         if(r0 < 0)
-            memset(klicensee, 0, 0x10);
-         else
-            memcpy(klicensee, decbuf.rif_key, 0x10);
+         dec_rif_key(&decbuf, klicensee);
       }
       else
       {
          memcpy(decbuf.primary_keys, act_data + 0x10, 0x20); //primary keys 1 and 2
 
-         int r0 = sceSblAuthMgrDecBindData_41daea12(decbuf.rif_key, 0x10, decbuf.primary_keys, 0x90, 0);
+         memcpy(decbuf.rif_key, rif_data + 0xA0, 0x10);
 
-         if(r0 < 0)
-            memset(klicensee, 0, 0x10);
-         else
-            memcpy(klicensee, decbuf.rif_key, 0x10);
+         dec_rif_key(&decbuf, klicensee);
       }
    }
    else
    {
-      memcpy(decbuf.primary_keys, cmd56_handshake, 0x20);
-
-      if(r4 == 0)
+      if(lic_derived_flag == 0)
       {
-         int r0 = sceSblAuthMgrDecBindData_41daea12(decbuf.rif_key, 0x10, decbuf.primary_keys, 0x90, 0);
+         memcpy(decbuf.primary_keys, cmd56_handshake, 0x20);
 
-         if(r0 < 0)
-            memset(klicensee, 0, 0x10);
-         else
-            memcpy(klicensee, decbuf.rif_key, 0x10);
+         memcpy(decbuf.rif_key, rif_data + 0xA0, 0x10);
+
+         dec_rif_key(&decbuf, klicensee);
       }
       else
       { 
+         memcpy(decbuf.primary_keys, cmd56_handshake, 0x20);
+
          memcpy(decbuf.rif_key, decbuf.rif_data + 0x50, 0x10); //rif key
 
          memset(decbuf.rif_data + 0x50, 0, 0x10); //clear rif key
          
-         int r0 = sceSblAuthMgrDecBindData_41daea12(decbuf.rif_key, 0x10, decbuf.primary_keys, 0x90, 0);
-
-         if(r0 < 0)
-            memset(klicensee, 0, 0x10);
-         else
-            memcpy(klicensee, decbuf.rif_key, 0x10);
+         dec_rif_key(&decbuf, klicensee);
       }
    }
 }
