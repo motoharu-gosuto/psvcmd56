542452b01d36b151faecd806560348d3c5b0574b
motoharu-gosuto <motoharu.gosuto@gmail.com>
2017-08-20 17:53:37 +0400

    add more code for get_sha224_digest_source_validate_card_init_f00D_C8D5FC

--- src/CMD56Reversed/SceMsif.cpp
+++ src/CMD56Reversed/SceMsif.cpp
@@ -27,7 +27,7 @@ int ms_execute_ex_set_cmd_read_short_data_C8A448(SceMsif_subctx *subctx, int cmd
    return 0;
 }
 
-int execute_f00d_command_2_rmauth_sm_C8D988(const char input[8])
+int execute_f00d_command_2_rmauth_sm_C8D988(const char input[0x10])
 {
    return 0;
 }
@@ -36,6 +36,17 @@ int w_dmac5_command_0x41_C8D2F0(char* result, const char* data, int size)
 {
    return 0;
 }
+
+#pragma pack(push,1)
+
+typedef struct dmac5_41_req1 //size is 0x28
+{
+   char F00D_cmd2_buffer_98[0x10];
+   char var_88[0x10];
+   char rand_value[8];
+}dmac5_41_req1;
+
+#pragma pack(pop)
 
 int get_sha224_digest_source_validate_card_init_f00D_C8D5FC(SceMsif_subctx *subctx, char *sha224_digest_source)
 {
@@ -46,12 +57,13 @@ int get_sha224_digest_source_validate_card_init_f00D_C8D5FC(SceMsif_subctx *subc
       return fdres1;
 
    //----------------
+   dmac5_41_req1 d5req1;
 
    //generate random number
-   char rand_value[8];
-   memset(rand_value, 0, 8);
+   
+   memset(d5req1.rand_value, 0, 8);
 
-   int rdgenres1 = SceSblSsMgrForDriver_4dd1b2e5(rand_value, 8, 1);
+   int rdgenres1 = SceSblSsMgrForDriver_4dd1b2e5(d5req1.rand_value, 8, 1);
    if(rdgenres1 < 0)
       return rdgenres1;
 
@@ -60,7 +72,7 @@ int get_sha224_digest_source_validate_card_init_f00D_C8D5FC(SceMsif_subctx *subc
    //save rand to temp
    char rand_B8[8];
    memset(rand_B8, 0, 8);
-   memcpy(rand_B8, rand_value, 0);
+   memcpy(rand_B8, d5req1.rand_value, 0);
 
    //prepare cmd 0x48 request
    char cmd48_src[0x20];
@@ -85,42 +97,71 @@ int get_sha224_digest_source_validate_card_init_f00D_C8D5FC(SceMsif_subctx *subc
 
    //----------------
 
-   char F00D_cmd2_buffer_98[0x10];
-   memset(F00D_cmd2_buffer_98, 0, 0x10);
-
-   F00D_cmd2_buffer_98[0] = cmd49_dest[0];
-   F00D_cmd2_buffer_98[4] = cmd49_dest[4]; 
-   F00D_cmd2_buffer_98[8] = cmd49_dest[8];
-   F00D_cmd2_buffer_98[C] = cmd49_dest[C];
+   memset(d5req1.F00D_cmd2_buffer_98, 0, 0x10);
+   memcpy(d5req1.F00D_cmd2_buffer_98, cmd49_dest, 0x10); //copy first 0x10 bytes of 0x49 resp
 
    //
 
-   char var_88[0x10];
-   memset(var_88, 0, 0x10);
+   memset(d5req1.var_88, 0, 0x10);
+   memcpy(d5req1.var_88, cmd49_dest + 0x10, 0x10); //copy second 0x10 bytes of 0x49 resp
 
-   var_88[0] = (cmd49_dest + 0x10)[0];
-   var_88[4] = (cmd49_dest + 0x10)[4];
-
-   (var_88 + 8)[0] = (cmd49_dest + 0x18)[0];
-   (var_88 + 8)[4] = (cmd49_dest + 0x18)[4];
-
-   int var_DC = *(var_88 + 7);
+   int var_DC = *(d5req1.var_88 + 7);
 
    //
 
    char var_C8[8];
    memset(var_C8, 0, 8);
-
-   var_C8[0] = (cmd49_dest + 0x20)[0];
-   var_C8[4] = (cmd49_dest + 0x20)[4];
+   memcpy(var_C8, cmd49_dest + 0x20, 8); //copy last 8 bytes of 0x49 resp
 
    //
 
-   int fdres2 = execute_f00d_command_2_rmauth_sm_C8D988(F00D_cmd2_buffer_98);
+   int fdres2 = execute_f00d_command_2_rmauth_sm_C8D988(d5req1.F00D_cmd2_buffer_98);
    if(fdres2 < 0)
       return fdres2;
 
    //----------------
+
+   char dmac5_result_1[0x8];
+   memset(dmac5_result_1, 0, 0x8);
+
+   //send dmac5 request with 0x20 bytes of 0x49 response and 8 bytes of random data
+   int dmc5res1 = w_dmac5_command_0x41_C8D2F0(dmac5_result_1, (char*)&d5req1, 0x28);
+   if(dmc5res1 != 0)
+      return dmc5res1;
+
+   //----------------
+
+   //verify hash, signature ?
+
+   if(memcmp(dmac5_result_1, var_C8, 8) != 0)
+      return -1; //returns not exactly this, but we dont care here
+
+   //----------------
+ 
+   char dmac_5_result_2[0x8];
+   memset(dmac_5_result_2, 0, 0x8);
+
+   //
+
+   char dmac5_input[0x10];
+   memset(dmac5_input, 0, 0x10);
+
+   memcpy(dmac5_input, d5req1.rand_value, 8);
+   memcpy(dmac5_input+8, d5req1.var_88 + 8, 8);
+
+   //
+
+   int dmc5res2 = w_dmac5_command_0x41_C8D2F0(dmac_5_result_2, dmac5_input, 0x10);
+   if(dmc5res2 != 0)
+      return dmc5res2;
+   
+   //----------------
+
+
+
+
+
+
 
    char cmd4A_src[0x20];
    memset(cmd4A_src, 0, 0x20);
