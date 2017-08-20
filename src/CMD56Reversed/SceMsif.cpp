952a274da36fb114716badc31b2abfccfaa70595
motoharu-gosuto <motoharu.gosuto@gmail.com>
2017-08-20 18:21:10 +0400

    simplify get_sha224_digest_source_validate_card_init_f00D_C8D5FC

--- src/CMD56Reversed/SceMsif.cpp
+++ src/CMD56Reversed/SceMsif.cpp
@@ -41,7 +41,7 @@ int w_dmac5_command_0x41_C8D2F0(char* result, const char* data, int size)
 
 typedef struct dmac5_41_req1 //size is 0x28
 {
-   char F00D_cmd2_buffer_98[0x10];
+   char f00d_cmd2_buffer[0x10];
    char var_88[0x10];
    char rand_value[8];
 }dmac5_41_req1;
@@ -50,37 +50,34 @@ typedef struct dmac5_41_req1 //size is 0x28
 
 int get_sha224_digest_source_validate_card_init_f00D_C8D5FC(SceMsif_subctx* subctx, char* sha224_digest_source)
 {
-   int cmd1_res = 0;
+   int f00d_cmd1_res = 0;
 
-   int fdres1 = food_execute_f00d_command_1_rmauth_sm_C8D908(&cmd1_res);
+   int fdres1 = food_execute_f00d_command_1_rmauth_sm_C8D908(&f00d_cmd1_res);
    if(fdres1 < 0)
       return fdres1;
 
-   //----------------
-   dmac5_41_req1 d5req1;
-
    //generate random number
-   
-   memset(d5req1.rand_value, 0, 8);
+   char rand_value[8];
+   memset(rand_value, 0, 8);
 
-   int rdgenres1 = SceSblSsMgrForDriver_4dd1b2e5(d5req1.rand_value, 8, 1);
+   int rdgenres1 = SceSblSsMgrForDriver_4dd1b2e5(rand_value, 8, 1);
    if(rdgenres1 < 0)
       return rdgenres1;
 
    //----------------
 
    //save rand to temp
-   char rand_B8[8];
-   memset(rand_B8, 0, 8);
-   memcpy(rand_B8, d5req1.rand_value, 0);
+   char rand_temp[8];
+   memset(rand_temp, 0, 8);
+   memcpy(rand_temp, rand_value, 0);
 
    //prepare cmd 0x48 request
    char cmd48_src[0x20];
    memset(cmd48_src, 0, 0x20);
 
    //8 + 1 + 0x17 = 0x20 total in bytes
-   memcpy(cmd48_src, rand_B8, 8); //copy rand
-   cmd48_src[8] = cmd1_res; //copy cmd1 resp 1 byte
+   memcpy(cmd48_src, rand_temp, 8); //copy rand
+   cmd48_src[8] = f00d_cmd1_res; //copy cmd1 resp 1 byte
    memset(cmd48_src + 9, 0, 0x17); //fill rest with 0
 
    //execute command 48
@@ -96,16 +93,14 @@ int get_sha224_digest_source_validate_card_init_f00D_C8D5FC(SceMsif_subctx* subc
    int res49 = ms_execute_ex_set_cmd_read_short_data_C8A448(subctx, MS_TPC_49, 0x40, cmd49_dest, 1000); //gets 0x40 bytes of response
 
    //----------------
-
-   memset(d5req1.F00D_cmd2_buffer_98, 0, 0x10);
-   memcpy(d5req1.F00D_cmd2_buffer_98, cmd49_dest, 0x10); //copy first 0x10 bytes of 0x49 resp
+   char f00d_cmd2_buffer[0x10];
+   memset(f00d_cmd2_buffer, 0, 0x10);
+   memcpy(f00d_cmd2_buffer, cmd49_dest, 0x10); //copy first 0x10 bytes of 0x49 resp
 
    //
-
-   memset(d5req1.var_88, 0, 0x10);
-   memcpy(d5req1.var_88, cmd49_dest + 0x10, 0x10); //copy second 0x10 bytes of 0x49 resp
-
-   int var_DC = *(d5req1.var_88 + 7);
+   char var_88[0x10];
+   memset(var_88, 0, 0x10);
+   memcpy(var_88, cmd49_dest + 0x10, 0x10); //copy second 0x10 bytes of 0x49 resp   
 
    //
 
@@ -115,7 +110,7 @@ int get_sha224_digest_source_validate_card_init_f00D_C8D5FC(SceMsif_subctx* subc
 
    //
 
-   int fdres2 = execute_f00d_command_2_rmauth_sm_C8D988(d5req1.F00D_cmd2_buffer_98); //sends first 0x10 bytes of 0x49 resp to f00d
+   int fdres2 = execute_f00d_command_2_rmauth_sm_C8D988(f00d_cmd2_buffer); //sends first 0x10 bytes of 0x49 resp to f00d
    if(fdres2 < 0)
       return fdres2;
 
@@ -123,6 +118,11 @@ int get_sha224_digest_source_validate_card_init_f00D_C8D5FC(SceMsif_subctx* subc
 
    char dmac5_result_1[0x8];
    memset(dmac5_result_1, 0, 0x8);
+
+   dmac5_41_req1 d5req1;
+   memcpy(d5req1.f00d_cmd2_buffer, f00d_cmd2_buffer, 0x10);
+   memcpy(d5req1.var_88, var_88, 0x10);
+   memcpy(d5req1.rand_value, rand_value, 0x8);
 
    int dmc5res1 = w_dmac5_command_0x41_C8D2F0(dmac5_result_1, (char*)&d5req1, 0x28); //send dmac5 request with 0x20 bytes of 0x49 response and 8 bytes of random data
    if(dmc5res1 != 0)
@@ -175,7 +175,7 @@ int get_sha224_digest_source_validate_card_init_f00D_C8D5FC(SceMsif_subctx* subc
    if(sha224_digest_source == 0)
       return -1; //returns not exactly this, but we dont care here
 
-   memcpy(sha224_digest_source, d5req1.F00D_cmd2_buffer_98, 0x10); //copy result
+   memcpy(sha224_digest_source, d5req1.f00d_cmd2_buffer, 0x10); //copy result
    
-   return var_DC;
+   return (*(var_88 + 7));
 }
