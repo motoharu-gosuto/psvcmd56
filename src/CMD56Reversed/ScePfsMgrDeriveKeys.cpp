10e14a1e3750ef13c9fb9eaa27f06512d2de66e2
motoharu-gosuto <motoharu.gosuto@gmail.com>
2017-10-04 03:24:27 +0400

    finalize derive_keys_from_klicensee_219B4A0

--- src/CMD56Reversed/ScePfsMgrDeriveKeys.cpp
+++ src/CMD56Reversed/ScePfsMgrDeriveKeys.cpp
@@ -27,11 +27,11 @@ typedef struct pfs_pmi_buffer_list_ctx
     char unk_91;
     
     uint16_t type;   //0x92
-    uint16_t unk_94; //0x94 = 0
+    uint16_t flag; //0x94 = 0
     
     char klicensee[0x10]; //0x96
     
-    uint16_t unk_A6; // = 0
+    uint16_t key_id; // = 0
     uint32_t unk_A8; // = 0
     uint32_t unk_AC; // = 0
     
@@ -54,14 +54,14 @@ typedef struct pfs_pmi_buffer_list_ctx
 
 typedef struct CryptEngineData //size is 0x60
 {
-   uint32_t unk_0;
+   const char* klicensee;
    uint32_t unk_4;
-   uint32_t unk_8;
-   uint16_t unk_C;
+   uint32_t salt;
+   uint16_t type;
    uint16_t flag; // 0xE - flag that selects decryption type ?
    
-   uint16_t key_id; //0x10
-   uint16_t seed1_base; //iv seed
+   uint16_t key_id; // 0x10
+   uint16_t seed1_base; // 0x12 iv seed
    
    uint32_t unk_14;
    uint32_t unk_18;
@@ -69,7 +69,7 @@ typedef struct CryptEngineData //size is 0x60
    
    uint32_t unk_20;
    uint32_t unk_24;
-   uint32_t size1;
+   uint32_t size1; //0x28
    char key[0x10]; //0x2C
    
    char iv_xor_key[0x10]; //0x3C
@@ -124,139 +124,116 @@ typedef struct CryptEngineWorkCtx //size is 0x18
    
 }CryptEngineWorkCtx;
 
-int combine_klicensee_digest_219E1D8(char *sha1_combined_digest, char *klicensee, uint32_t unk2, uint16_t flags, uint32_t salt, uint16_t arg_4)
+int combine_klicensee_digest_219E1D8(char* sha1_combined_digest, const char* klicensee, uint32_t unk2, uint16_t flags, uint32_t salt, uint16_t arg_4)
 {
    return 0;
 }
 
-int calculate_sha1_chain_219E1CC(char *result0, char *result1, char *klicensee, uint32_t ignored, uint32_t salt)
+int calculate_sha1_chain_219E1CC(char* result0, char* result1, const char* klicensee, uint32_t ignored, uint32_t salt)
 {
    return 0;
 }
 
-int hmac1_sha1_or_sha1_chain_219E0DC(char *result0, char *result1, char *klicensee, uint32_t unk3, uint16_t arg_0, uint32_t salt, uint16_t unused_arg_8)
+int hmac1_sha1_or_sha1_chain_219E0DC(char* result0, char* result1, const char* klicensee, uint32_t unk3, uint16_t arg_0, uint32_t salt, uint16_t unused_arg_8)
 {
    return 0;
 }
 
-int hmac_sha1_219E164(char *result0, char *result1, char *klicensee, uint16_t unk3, uint16_t arg_0, void *data, uint32_t data_len)
+int hmac_sha1_219E164(char* result0, char* result1, const char* klicensee, uint16_t unk3, uint16_t arg_0, const char* data, uint32_t data_len)
 {
    return 0;
 }
 
-typedef struct derive_keys_r7_ctx
+struct derive_keys_ctx;
+
+typedef int(derive_keys_sub_219A29C_cb)(const derive_keys_ctx*);
+
+typedef struct derive_keys_ctx
 {
+   derive_keys_sub_219A29C_cb* unk_14; //function pointer
+
    uint32_t unk_40;
 
 
    uint32_t unk_58;
+   uint32_t unk_68;
 
-}derive_keys_r7_ctx;
+   char unk_84[];
 
-int derive_subroutine(int r2, CryptEngineData* r4, pfs_pmi_buffer_list_ctx * r5, char* r6, derive_keys_r7_ctx* r7, uint32_t r8)
+}derive_keys_ctx;
+
+int sub_219A29C(const derive_keys_ctx* ctx)
 {
-   int some_flag = 0xC0000B03 & (1 << r2);
+   return ctx->unk_68;
+}
 
-   if((r2 > 0x1F) || (some_flag == 0))
+typedef struct ctx_12f8d58e
+{
+   uint32_t unk_28;
+   uint32_t unk_2C;
+
+}ctx_12f8d58e;
+
+ctx_12f8d58e* SceIofilemgrForDriver_thread_related_12f8d58e()
+{
+   return 0;
+}
+
+int derive_keys_from_klicensee_219B4A0(CryptEngineData *data, int salt, int unk2, int unk3, uint16_t seed1_base, int arg_4, const derive_keys_ctx* drv_ctx, const pfs_pmi_buffer_list_ctx *pfs_pmi_bcl)
+{
+   ctx_12f8d58e* ctx0 = SceIofilemgrForDriver_thread_related_12f8d58e();
+   data->unk_18 = 0x100;
+   if((ctx0 != 0) && (ctx0->unk_2C >= 0))
+      data->unk_18 = (uint32_t)(ctx0->unk_2C & 0x0000FFFF);
+
+   ctx_12f8d58e* ctx1 = SceIofilemgrForDriver_thread_related_12f8d58e();
+   data->unk_14 = 0xF;
+   if(ctx1 != 0)
+      data->unk_14 = ctx1->unk_28;
+
+   data->unk_4 = pfs_pmi_bcl->unk_A8;
+   data->salt = salt;
+   data->unk_1C = arg_4;
+
+   data->type = pfs_pmi_bcl->type;
+   data->flag = pfs_pmi_bcl->flag;
+   data->key_id = pfs_pmi_bcl->key_id;
+   data->seed1_base = seed1_base;
+   
+   data->unk_20 = unk2;
+   data->unk_24 = unk3;
+
+   data->klicensee = pfs_pmi_bcl->klicensee;
+
+   data->size1 = drv_ctx->unk_14(drv_ctx);
+   
+   int some_flag_base = (uint32_t)(data->flag - 2);
+   int some_flag = 0xC0000B03 & (1 << some_flag_base);
+
+   if((some_flag_base > 0x1F) || (some_flag == 0))
    {
-      calculate_sha1_chain_219E1CC(r4->key, r4->iv_xor_key, r6, r5->unk_A8, r8);
-      return combine_klicensee_digest_219E1D8(r4->hmac_key, r6, r5->unk_A8, r5->unk_94, r8, r5->unk_A6);
+      calculate_sha1_chain_219E1CC(data->key, data->iv_xor_key, pfs_pmi_bcl->klicensee, pfs_pmi_bcl->unk_A8, salt);
+      return combine_klicensee_digest_219E1D8(data->hmac_key, pfs_pmi_bcl->klicensee, pfs_pmi_bcl->unk_A8, pfs_pmi_bcl->flag, salt, pfs_pmi_bcl->key_id);
    }
    else
    {
-      if((r7->unk_40 != 0 && r7->unk_40 != 3) || (r7->unk_58 <= 1))
+      if((drv_ctx->unk_40 != 0 && drv_ctx->unk_40 != 3) || (drv_ctx->unk_58 <= 1))
       {    
-         hmac1_sha1_or_sha1_chain_219E0DC(r4->key, r4->iv_xor_key, r6, r5->unk_A8, r5->unk_94, r8, r5->unk_A6);
-         return combine_klicensee_digest_219E1D8(r4->hmac_key, r6, r5->unk_A8, r5->unk_94, r8, r5->unk_A6);
+         hmac1_sha1_or_sha1_chain_219E0DC(data->key, data->iv_xor_key, pfs_pmi_bcl->klicensee, pfs_pmi_bcl->unk_A8, pfs_pmi_bcl->flag, salt, pfs_pmi_bcl->key_id);
+         return combine_klicensee_digest_219E1D8(data->hmac_key, pfs_pmi_bcl->klicensee, pfs_pmi_bcl->unk_A8, pfs_pmi_bcl->flag, salt, pfs_pmi_bcl->key_id);
       }
       else
       {
-         if(r2 == 0 || r2 == 3)
+         if(drv_ctx->unk_40 == 0 || drv_ctx->unk_40 == 3)
          {
-            hmac_sha1_219E164(r4->key, r4->iv_xor_key, r6, r5->unk_94, r5->unk_A6, r7 + 0x84, 0x14);
-            return combine_klicensee_digest_219E1D8(r4->hmac_key, r6, r5->unk_A8, r5->unk_94, r8, r5->unk_A6);
+            hmac_sha1_219E164(data->key, data->iv_xor_key, pfs_pmi_bcl->klicensee, pfs_pmi_bcl->flag, pfs_pmi_bcl->key_id, drv_ctx->unk_84, 0x14);
+            return combine_klicensee_digest_219E1D8(data->hmac_key, pfs_pmi_bcl->klicensee, pfs_pmi_bcl->unk_A8, pfs_pmi_bcl->flag, salt, pfs_pmi_bcl->key_id);
          }
          else
          {
-            hmac_sha1_219E164(r4->key, r4->iv_xor_key, r6, r5->unk_94, r5->unk_A6, 0, 0x14);
-            return combine_klicensee_digest_219E1D8(r4->hmac_key, r6, r5->unk_A8, r5->unk_94, r8, r5->unk_A6);
+            hmac_sha1_219E164(data->key, data->iv_xor_key, pfs_pmi_bcl->klicensee, pfs_pmi_bcl->flag, pfs_pmi_bcl->key_id, 0, 0x14);
+            return combine_klicensee_digest_219E1D8(data->hmac_key, pfs_pmi_bcl->klicensee, pfs_pmi_bcl->unk_A8, pfs_pmi_bcl->flag, salt, pfs_pmi_bcl->key_id);
          }
       }
    }
-}
-
-int derive_keys_from_klicensee_219B4A0(CryptEngineData *data, int salt, int unk2, int unk3, int arg_0, int arg_4, derive_keys_r7_ctx* arg_8, pfs_pmi_buffer_list_ctx *pfs_pmi_bcl)
-{
-   int r0 = data;
-   int r1 = salt;
-   int r2 = unk2;
-   int r3 = unk3;
-
-   int r4 = r0;
-   int r8 = r1;
-
-   //VMOV            D8, R2, R3
-   
-   short r10 = arg_0;
-   int r7 = arg_8;
-   int r5 = pfs_pmi_bcl;
-   
-   int r0 = ScePfsMgr.SceIofilemgrForDriver._imp_thread_related_12f8d58e();
-   int r3 = 0x100;
-   
-   if(r0 != 0)
-   {
-      int r3 = r0[0x2C];
-      if(r3 >= 0)
-      {
-         //UXTH          R3, R3
-      }
-   }
-
-   r4[0x18] = r3;
-
-   int r0 = ScePfsMgr.SceIofilemgrForDriver._imp_thread_related_12f8d58e();
-   
-   int r3 = 0xF;
-
-   if(r0 != 0)
-   {
-      int r3 = r0[0x28];
-   }
-
-   short r12 = r5[0x92] ;
-   int r6 = r5 + 0x96;
-   short lr = r5[0x94];
-   int r0 = r7;
-   short r1 = r5[0xA6];
-   int r2 = r5[0xA8];
-
-   r4[0x14] = r3;
-
-   int r3 = arg_4;
-
-   r4[4] = r2;
-   r4[8] = r8;
-   r4[0x1C] = r3;
-
-   r4[0xC] = (short) r12;
-   r4[0xE] = (short)lr;
-   r4[0x12] = (short) r10;
-   r4[0x10] = (short) r1;
-   
-   //VSTR            D8, [R4,#0x20]
-
-   r4[0x00] = r6;
-
-   int r3 = r7[0x14]
-
-   int r0 = r3(); //call callback
-   
-   short r2 = r4[0xE];
-   r4[0x28] = r0;
-   
-   int r2 = r2 - 2;
-   //UXTH            R2, R2
-
-   return derive_subroutine(r2, r4, r5, r6, r8);
 }
