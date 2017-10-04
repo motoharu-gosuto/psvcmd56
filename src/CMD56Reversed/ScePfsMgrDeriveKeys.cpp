ecf4ad183855ca12c54813481df8ebc631ff760a
motoharu-gosuto <motoharu.gosuto@gmail.com>
2017-10-05 00:34:18 +0400

    rewrite hmac_sha1_219E164 in raw C

--- src/CMD56Reversed/ScePfsMgrDeriveKeys.cpp
+++ src/CMD56Reversed/ScePfsMgrDeriveKeys.cpp
@@ -228,37 +228,41 @@ int hmac1_sha1_or_sha1_chain_219E0DC(char* key, char* iv_xor_key, const char* kl
    return 0;
 }
 
-int hmac_sha1_219E164(char* key, char* iv_xor_key, const char* klicensee, uint16_t flag, uint16_t key_id, const char* data, uint32_t data_len)
+int hmac_sha1_219E164(char* key, char* iv_xor_key, const char* klicensee, uint16_t flag, uint16_t ignored_key_id, const char* data, uint32_t data_len)
 {
-   /*
-   MOV             R5, R1  ; char*
-   MOVW            R1, #(hmac_key_21A93C8 AND 0xFFFF) ; 021A93C8 data reference 8B2562E4
+   int r5 = r1;
+   int r1 = &hmac_key_21A93C8;
+   int r7 = r0;
+   int r8 = r2;
    
-   MOV             R7, R0
-   MOV             R8, R2  ; char*
-   LDRD.W          R2, R3, [SP,#0x38+data] ; void *data, int data_len
-   MOVT.W          R1, #high16(hmac_key_21A93C8) ; 021A93C8 data reference 8B2562E4
+   int r2 = data;
+   int r3 = data_len;
+
+   int r0 = sp;
+   int r4 = sp;
+
+   hmacSha1Digest_219DE68(r0, r1, r2, r3);
+
+   r4[0x0] = r0;
+   r4[0x4] = r1;
+   r4[0x8] = r2;
+   r4[0xC] = r3;
+
+   int r10 = r8[0x0];
+   int r9 =  r8[0x4];
+   int r12 = r8[0x8];
+   int lr =  r8[0xC];
    
-   MOV             R0, SP  ; digest
-   MOV             R4, SP
-   BL              hmacSha1Digest_219DE68
-   LDMIA           R4!, {R0-R3} ; get digest data
-   LDR.W           R10, [R8]
-   LDR.W           R9, [R8,#4]
-   LDR.W           R12, [R8,#8]
-   LDR.W           LR, [R8,#0xC]
+   r7[0x0] = r10;
+   r7[0x4] = r9;
+   r7[0x8] = r12;
+   r7[0xC] = lr;
    
-   STR.W           R10, [R7] ; store result
-   STR.W           R9, [R7,#4] ; store result
-   STR.W           R12, [R7,#8] ; store result
-   STR.W           LR, [R7,#0xC] ; store result
-   STR             R0, [R5] ; store result
-   
-   STR             R1, [R5,#4] ; store result
-   STR             R2, [R5,#8] ; store result
-   
-   STR             R3, [R5,#0xC] ; store result
-   */
+   r5[0x0] = r0;
+   r5[0x4] = r1;
+   r5[0x8] = r2;
+   r5[0xC] = r3;
+
    return 0;
 }
 
