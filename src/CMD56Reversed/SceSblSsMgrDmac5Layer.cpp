0377331436e648af6d452e6c7f88cab4e1655644
motoharu-gosuto <motoharu.gosuto@gmail.com>
2017-10-20 02:37:30 +0400

    implement SceSblSsMgrForDriver_sceSblSsMgrSHA1ForDriver_eb3af9b5 mark with keygen functions as unimplemented

--- src/CMD56Reversed/SceSblSsMgrDmac5Layer.cpp
+++ src/CMD56Reversed/SceSblSsMgrDmac5Layer.cpp
@@ -6,19 +6,19 @@
 
 int SceSblSsMgrForDriver_sceSblSsMgrAESCBCDecryptWithKeygenForDriver_1901cb5e(const unsigned char* src, unsigned char* dst, int size, const unsigned char* key, int key_size, unsigned char* iv, uint16_t key_id, int mask_enable)
 {
-   return 0;
+   throw std::runtime_error("unsupported keygen function");
 }
 
 int SceSblSsMgrForDriver_sceSblSsMgrAESCBCEncryptWithKeygenForDriver_711c057a(const unsigned char* src, unsigned char* dst, int size, const unsigned char* key, int key_size, unsigned char* iv, uint16_t key_id, int mask_enable)
 {
-   return 0;
+   throw std::runtime_error("unsupported keygen function");
 }
 
 //
 
 int SceSblSsMgrForDriver_sceSblSsMgrAESECBEncryptWithKeygenForDriver_0f7d28af(const unsigned char* src, unsigned char* dst, int size, const unsigned char* key, int key_size, uint16_t key_id, int mask_enable)
 {
-   return 0;
+   throw std::runtime_error("unsupported keygen function");
 }
 
 //---------------------
@@ -54,11 +54,22 @@ int SceSblSsMgrForDriver_sceSblSsMgrAESCMACForDriver_1b14658d(const unsigned cha
 
 int SceSblSsMgrForDriver_sceSblSsMgrAESCMACWithKeygenForDriver_83b058f5(const unsigned char* src, unsigned char* dst, int size, const unsigned char* key, int key_size, unsigned char* iv, uint16_t key_id, int mask_enable, int command_bit)
 {
-   return 0;
+   throw std::runtime_error("unsupported keygen function");
 }
 
 int SceSblSsMgrForDriver_sceSblSsMgrSHA1ForDriver_eb3af9b5(const unsigned char* src, unsigned char* dst, int size, unsigned char* iv, int mask_enable, int command_bit)
 {
+   if(iv != 0)
+      throw std::runtime_error("unsupported iv");
+
+   if(mask_enable != 1)
+      throw std::runtime_error("unsupported mask_enable");
+
+   if(command_bit != 0)
+      throw std::runtime_error("unsupported command_bit");
+
+   sha1(src, size, dst);
+
    return 0;
 }
 
