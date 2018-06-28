e85b7d2875fccb631d1a4a423053287cccb9bada
motoharu-gosuto <motoharu.gosuto@gmail.com>
2018-06-29 02:01:02 +0400

    add f00d keyring implementation

---
+++ src/CMD56Reversed/F00D/SceSblDMAC5DmacKRBase.cpp
@@ -0,0 +1,71 @@
+#include "SceSblDMAC5DmacKRBase.h"
+
+#include <stdexcept>
+
+#include "F00DKeyEncryptorService.h"
+
+using namespace f00d;
+
+SceDmacmgrKeyringReg_t g_f00d_key_slots;
+
+//set key into slot directly - works for slots 0x0-0x7 and 0x1D
+int SceSblDMAC5DmacKRBase::set_key(unsigned char* key, int key_size, int slot_id)
+{
+   if(key == 0)
+      return -1;
+
+   if(key_size > DMAC5_KEYSIZE)
+      return -1;
+
+   if(slot_id > DMAC5_MAX_SLOT_ID)
+      return -1;
+
+   switch(slot_id)
+   {
+   case 0:
+   case 1:
+   case 2:
+   case 3:
+   case 4:
+   case 5:
+   case 6:
+   case 7:
+   case 0x1D:
+      memset(g_f00d_key_slots.keys.all_slots.slots[slot_id], 0, DMAC5_KEYSIZE);
+      memcpy(g_f00d_key_slots.keys.all_slots.slots[slot_id], key, key_size);
+      return 0;
+   default:
+      return -1;
+   }
+}
+
+//encrypt the key with f00d key with key_id. then set result to slot_id
+int SceSblDMAC5DmacKRBase::set_key(unsigned char* key, int key_size, int slot_id, int key_id)
+{
+   if(key == 0)
+      return -1;
+
+   if(key_size > DMAC5_KEYSIZE)
+      return -1;
+
+   if(slot_id > DMAC5_MAX_SLOT_ID)
+      return -1;
+
+   switch(key_id)
+   {
+   //only key_id 0 is supported. it is handled by F00DKeyEncryptorService
+   case 0:
+      {
+         unsigned char drv_key[0x20] = {0}; //use max possible buffer
+
+         //encrypt the key
+         auto encryptor = F00DKeyEncryptorService::get();
+         if(encryptor->encrypt_key(key, key_size, drv_key) < 0)
+            return -1;
+
+         return set_key(drv_key, key_size, slot_id);
+      }
+   default:
+      throw std::runtime_error("not implemented");
+   }
+}
---
+++ src/CMD56Reversed/F00D/SceSblDMAC5DmacKRBase.h
@@ -0,0 +1,79 @@
+#pragma once
+
+#define DMAC5_KEYSIZE 0x20
+#define DMAC5_MAX_SLOT_ID 0x1F
+
+//Key slots 0x0-0x7 and 0x1D can be modified directly using dmac5keyring. 
+#define DMAC5_KEYRING_KEY_0 0
+#define DMAC5_KEYRING_KEY_1 1
+#define DMAC5_KEYRING_KEY_2 2
+#define DMAC5_KEYRING_KEY_3 3
+#define DMAC5_KEYRING_KEY_4 4
+#define DMAC5_KEYRING_KEY_5 5
+#define DMAC5_KEYRING_KEY_6 6
+#define DMAC5_KEYRING_KEY_7 7
+#define DMAC5_KEYRING_KEY_1D 0x1D
+
+namespace f00d
+{
+   #pragma pack(push,1)
+
+   typedef struct SceDmacmgrKeyringReg_t
+   {
+      union
+      {
+         struct 
+         {
+            char slot_00[DMAC5_KEYSIZE];
+            char slot_01[DMAC5_KEYSIZE];
+            char slot_02[DMAC5_KEYSIZE];
+            char slot_03[DMAC5_KEYSIZE];
+            char slot_04[DMAC5_KEYSIZE];
+            char slot_05[DMAC5_KEYSIZE];
+            char slot_06[DMAC5_KEYSIZE];
+            char slot_07[DMAC5_KEYSIZE];
+            char slot_08[DMAC5_KEYSIZE];
+            char slot_09[DMAC5_KEYSIZE];
+            char slot_0A[DMAC5_KEYSIZE];
+            char slot_0B[DMAC5_KEYSIZE];
+            char slot_0C[DMAC5_KEYSIZE];
+            char slot_0D[DMAC5_KEYSIZE];
+            char slot_0E[DMAC5_KEYSIZE];
+            char slot_0F[DMAC5_KEYSIZE];
+            char slot_10[DMAC5_KEYSIZE];
+            char slot_11[DMAC5_KEYSIZE];
+            char slot_12[DMAC5_KEYSIZE];
+            char slot_13[DMAC5_KEYSIZE];
+            char slot_14[DMAC5_KEYSIZE];
+            char slot_15[DMAC5_KEYSIZE];
+            char slot_16[DMAC5_KEYSIZE];
+            char slot_17[DMAC5_KEYSIZE];
+            char slot_18[DMAC5_KEYSIZE];
+            char slot_19[DMAC5_KEYSIZE];
+            char slot_1A[DMAC5_KEYSIZE];
+            char slot_1B[DMAC5_KEYSIZE];
+            char slot_1C[DMAC5_KEYSIZE];
+            char slot_1D[DMAC5_KEYSIZE];
+            char slot_1E[DMAC5_KEYSIZE];
+            char slot_1F[DMAC5_KEYSIZE];
+         } each_slot;
+         struct
+         {
+            char slots[DMAC5_MAX_SLOT_ID + 1][DMAC5_KEYSIZE];
+         } all_slots;
+      } keys;
+     
+   } SceDmacmgrKeyringReg_t;
+
+   #pragma pack(pop)
+
+   class SceSblDMAC5DmacKRBase
+   {
+   public:
+      //set key into slot directly - works for slots 0x0-0x7 and 0x1D
+      static int set_key(unsigned char* key, int key_size, int slot_id);
+
+      //encrypt the key with f00d key with key_id. then set result to slot_id
+      static int set_key(unsigned char* key, int key_size, int slot_id, int key_id);
+   };
+}
