61b6789b42e280e5b9bf309492655f017d891c93
motoharu-gosuto <motoharu.gosuto@gmail.com>
2018-07-15 18:07:51 +0400

    reverse edigest

--- src/CMD56Reversed/edigest.cpp
+++ src/CMD56Reversed/edigest.cpp
@@ -12,7 +12,7 @@
 #define __ROL4__ __ROL__
 #define __ROR4__ __ROR__
 
-int dword_4054F4[64] = 
+const int dword_4054F4[64] = 
 {
    0x428A2F98, 0x71374491, 0xB5C0FBCF, 0xE9B5DBA5, 0x3956C25B, 0x59F111F1, 0x923F82A4, 0xAB1C5ED5, 
    0xD807AA98, 0x12835B01, 0x243185BE, 0x550C7DC3, 0x72BE5D74, 0x80DEB1FE, 0x9BDC06A7, 0xC19BF174,
@@ -24,12 +24,158 @@ int dword_4054F4[64] =
    0x748F82EE, 0x78A5636F, 0x84C87814, 0x8CC70208, 0x90BEFFFA, 0xA4506CEB, 0xBEF9A3F7, 0xC67178F2,
 };
 
-//input buffer is 0x40 bytes (0x10 integers)
-void __cdecl crypto_or_hash_primitive_40D468(const int *input, int *digest2_result)
+//change digest_permu[9]
+//change digest_permu[13]
+void do_block0(const int* work_buffer, int* digest_permu, const int res_item_ctr, const int dgr_val0)
 {
-   //peform permutation of digest2
-   int digest_permu[16];
+   int cycle_acc;
 
+   cycle_acc = work_buffer[res_item_ctr]
+                  + dword_4054F4[res_item_ctr]
+                  + (digest_permu[10] ^ digest_permu[12] & (digest_permu[10] ^ digest_permu[11]))
+                  + digest_permu[9]
+                  + (__ROL4__(digest_permu[12], 7) ^ __ROL4__(digest_permu[12], 21) ^ __ROL4__(digest_permu[12], 26));
+
+   digest_permu[13] += cycle_acc;
+
+   digest_permu[9] = (dgr_val0 & digest_permu[15] | digest_permu[14] & (dgr_val0 | digest_permu[15]))
+                  + (__ROL4__(dgr_val0, 10) ^ __ROL4__(dgr_val0, 19) ^ __ROL4__(dgr_val0, 30))
+                  + cycle_acc;
+}
+
+//change digest_permu[10]
+//change digest_permu[14]
+void do_block1(const int* work_buffer, int* digest_permu, const int res_item_ctr, const int dgr_val0)
+{
+   int v7;
+
+   v7 = work_buffer[res_item_ctr + 1]
+            + dword_4054F4[res_item_ctr + 1]
+            + (digest_permu[11] ^ digest_permu[13] & (digest_permu[11] ^ digest_permu[12]))
+            + digest_permu[10]
+            + (__ROL4__(digest_permu[13], 7) ^ __ROL4__(digest_permu[13], 21) ^ __ROL4__(digest_permu[13], 26));
+
+   digest_permu[14] += v7;
+
+   digest_permu[10] = (dgr_val0 & digest_permu[9] | digest_permu[15] & (dgr_val0 | digest_permu[9]))
+                  + (__ROL4__(digest_permu[9], 10) ^ __ROL4__(digest_permu[9], 19) ^ __ROL4__(digest_permu[9], 30))
+                  + v7;
+}
+
+//change digest_permu[11]
+//change digest_permu[15]
+void do_block2(const int* work_buffer, int* digest_permu, const int res_item_ctr, const int dgr_val0)
+{
+   int v8;
+
+   v8 = work_buffer[res_item_ctr + 2]
+            + dword_4054F4[res_item_ctr + 2]
+            + (digest_permu[12] ^ digest_permu[14] & (digest_permu[12] ^ digest_permu[13]))
+            + digest_permu[11]
+            + (__ROL4__(digest_permu[14], 7) ^ __ROL4__(digest_permu[14], 21) ^ __ROL4__(digest_permu[14], 26));
+
+   digest_permu[15] += v8;
+
+   digest_permu[11] = (digest_permu[9] & digest_permu[10] | dgr_val0 & (digest_permu[9] | digest_permu[10]))
+                  + (__ROL4__(digest_permu[10], 10) ^ __ROL4__(digest_permu[10], 19) ^ __ROL4__(digest_permu[10], 30))
+                  + v8;
+}
+
+//change digest_permu[12]
+void do_block3(const int* work_buffer, int* digest_permu, const int res_item_ctr, const int dgr_val0, int& v10)
+{
+   int v9;
+
+   v9 = work_buffer[res_item_ctr + 3]
+            + dword_4054F4[res_item_ctr + 3]
+            + (digest_permu[13] ^ digest_permu[15] & (digest_permu[13] ^ digest_permu[14]))
+            + digest_permu[12]
+            + (__ROL4__(digest_permu[15], 7) ^ __ROL4__(digest_permu[15], 21) ^ __ROL4__(digest_permu[15], 26));
+
+   digest_permu[12] = (digest_permu[10] & digest_permu[11] | digest_permu[9] & (digest_permu[10] | digest_permu[11]))
+                  + (__ROL4__(digest_permu[11], 10) ^ __ROL4__(digest_permu[11], 19) ^ __ROL4__(digest_permu[11], 30))
+                  + v9;
+
+   v10 = v9 + dgr_val0;
+}
+
+//change digest_permu[9]
+//change digest_permu[13]
+void do_block4(const int* work_buffer, int* digest_permu, const int res_item_ctr, const int v10)
+{
+   int v11;
+
+   v11 = work_buffer[res_item_ctr + 4]
+            + dword_4054F4[res_item_ctr + 4]
+            + (digest_permu[14] ^ v10 & (digest_permu[14] ^ digest_permu[15]))
+            + digest_permu[13]
+            + (__ROL4__(v10, 7) ^ __ROL4__(v10, 21) ^ __ROL4__(v10, 26));
+
+   digest_permu[9] += v11;
+
+   digest_permu[13] = (digest_permu[11] & digest_permu[12] | digest_permu[10] & (digest_permu[11] | digest_permu[12]))
+                  + (__ROL4__(digest_permu[12], 10) ^ __ROL4__(digest_permu[12], 19) ^ __ROL4__(digest_permu[12], 30))
+                  + v11;
+}
+
+//change digest_permu[10]
+//change digest_permu[14]
+void do_block5(const int* work_buffer, int* digest_permu, const int res_item_ctr, const int v10)
+{
+   int v12;
+
+   v12 = work_buffer[res_item_ctr + 5]
+            + dword_4054F4[res_item_ctr + 5]
+            + (digest_permu[15] ^ digest_permu[9] & (v10 ^ digest_permu[15]))
+            + digest_permu[14]
+            + (__ROL4__(digest_permu[9], 7) ^ __ROL4__(digest_permu[9], 21) ^ __ROL4__(digest_permu[9], 26));
+
+   digest_permu[10] += v12;
+
+   digest_permu[14] = (digest_permu[12] & digest_permu[13] | digest_permu[11] & (digest_permu[12] | digest_permu[13]))
+                  + (__ROL4__(digest_permu[13], 10) ^ __ROL4__(digest_permu[13], 19) ^ __ROL4__(digest_permu[13], 30))
+                  + v12;
+}
+
+//change digest_permu[11]
+//change digest_permu[15]
+void do_block6(const int* work_buffer, int* digest_permu, const int res_item_ctr, const int v10)
+{
+   int v13;
+
+   v13 = work_buffer[res_item_ctr + 6]
+            + dword_4054F4[res_item_ctr + 6]
+            + (v10 ^ digest_permu[10] & (v10 ^ digest_permu[9]))
+            + digest_permu[15]
+            + (__ROL4__(digest_permu[10], 7) ^ __ROL4__(digest_permu[10], 21) ^ __ROL4__(digest_permu[10], 26));
+
+   digest_permu[11] += v13;
+
+   digest_permu[15] = (digest_permu[13] & digest_permu[14] | digest_permu[12] & (digest_permu[13] | digest_permu[14]))
+                  + (__ROL4__(digest_permu[14], 10) ^ __ROL4__(digest_permu[14], 19) ^ __ROL4__(digest_permu[14], 30))
+                  + v13;
+}
+
+//change digest_permu[12]
+void do_block7(const int* work_buffer, int* digest_permu, const int res_item_ctr, const int v10, int& dgr_val0)
+{
+   int v14;
+
+   v14 = work_buffer[res_item_ctr + 7]
+            + dword_4054F4[res_item_ctr + 7]
+            + (digest_permu[9] ^ digest_permu[11] & (digest_permu[9] ^ digest_permu[10]))
+            + (__ROL4__(digest_permu[11], 7) ^ __ROL4__(digest_permu[11], 21) ^ __ROL4__(digest_permu[11], 26))
+            + v10;
+
+   digest_permu[12] += v14;
+
+   dgr_val0 = (digest_permu[14] & digest_permu[15] | digest_permu[13] & (digest_permu[14] | digest_permu[15]))
+            + (__ROL4__(digest_permu[15], 10) ^ __ROL4__(digest_permu[15], 19) ^ __ROL4__(digest_permu[15], 30))
+            + v14;
+}
+
+void digest_permutation(const int *digest2_result, int* digest_permu)
+{
    digest_permu[1] =  digest2_result[7];
    digest_permu[9] =  digest2_result[7];
 
@@ -52,10 +198,11 @@ void __cdecl crypto_or_hash_primitive_40D468(const int *input, int *digest2_resu
    digest_permu[15] = digest2_result[1];
 
    digest_permu[5] =  digest2_result[0];
-   int dgr_val0 = digest2_result[0]; //maybe this acts as a digest_permu[0]
+}
 
+void construct_work_buffer(const int *input, int* work_buffer)
+{
    //copy input to head of the work buffer
-   int work_buffer[64];
 
    int in_ctr = 0;
    do
@@ -65,7 +212,7 @@ void __cdecl crypto_or_hash_primitive_40D468(const int *input, int *digest2_resu
    }
    while (in_ctr <= 0xF);
 
-   //derive tail of work buffer
+   //derive tail of work buffer from work buffer itself
 
    int wb_ctr = 0;
 
@@ -86,17 +233,45 @@ void __cdecl crypto_or_hash_primitive_40D468(const int *input, int *digest2_resu
       wb_ctr++;
    }
    while (wb_ctr <= 0x2F);
+}
 
-   //==================================
-   int cycle_acc;
-   int v7;
-   int v8;
-   int v9;
-   int v10;
-   int v11;
-   int v12;
-   int v13;
-   int v14;
+//input buffer is 0x40 bytes (0x10 integers)
+//digest2_result is 0x20 bytes (0x8 integers)
+void crypto_or_hash_primitive_40D468(const int *input, int *digest2_result)
+{
+   //peform permutation of digest2
+   int digest_permu[16];
+   digest_permutation(digest2_result, digest_permu);
+   
+   //construct work buffer
+   int work_buffer[64];
+   construct_work_buffer(input, work_buffer);
+   
+   //main processing - this will change digest_permu
+
+   int dgr_val0 = digest2_result[0];
+
+   int res_item_ctr = 0;
+
+   do
+   {
+      int v10;
+
+      do_block0(work_buffer, digest_permu, res_item_ctr, dgr_val0);
+      do_block1(work_buffer, digest_permu, res_item_ctr, dgr_val0);
+      do_block2(work_buffer, digest_permu, res_item_ctr, dgr_val0);
+      do_block3(work_buffer, digest_permu, res_item_ctr, dgr_val0, v10);
+
+      do_block4(work_buffer, digest_permu, res_item_ctr, v10);
+      do_block5(work_buffer, digest_permu, res_item_ctr, v10);
+      do_block6(work_buffer, digest_permu, res_item_ctr, v10);
+      do_block7(work_buffer, digest_permu, res_item_ctr, v10, dgr_val0);
+
+      res_item_ctr += 8;
+   }
+   while (res_item_ctr <= 0x3F);
+
+
    int res_acc0;
    int res_acc1;
    int res_acc2;
@@ -106,107 +281,6 @@ void __cdecl crypto_or_hash_primitive_40D468(const int *input, int *digest2_resu
    int res_acc6;
    int res_acc7;
    int res_acc8;
-
-   int res_item_ctr = 0;
-
-   do
-   {
-      cycle_acc = work_buffer[res_item_ctr]
-               + dword_4054F4[res_item_ctr]
-               + (digest_permu[10] ^ digest_permu[12] & (digest_permu[10] ^ digest_permu[11]))
-               + digest_permu[9]
-               + (__ROL4__(digest_permu[12], 7) ^ __ROL4__(digest_permu[12], 21) ^ __ROL4__(digest_permu[12], 26));
-
-      digest_permu[13] += cycle_acc;
-
-      digest_permu[9] = (dgr_val0 & digest_permu[15] | digest_permu[14] & (dgr_val0 | digest_permu[15]))
-                     + (__ROL4__(dgr_val0, 10) ^ __ROL4__(dgr_val0, 19) ^ __ROL4__(dgr_val0, 30))
-                     + cycle_acc;
-
-      v7 = work_buffer[res_item_ctr + 1]
-         + dword_4054F4[res_item_ctr + 1]
-         + (digest_permu[11] ^ digest_permu[13] & (digest_permu[11] ^ digest_permu[12]))
-         + digest_permu[10]
-         + (__ROL4__(digest_permu[13], 7) ^ __ROL4__(digest_permu[13], 21) ^ __ROL4__(digest_permu[13], 26));
-
-      digest_permu[14] += v7;
-
-      digest_permu[10] = (dgr_val0 & digest_permu[9] | digest_permu[15] & (dgr_val0 | digest_permu[9]))
-                     + (__ROL4__(digest_permu[9], 10) ^ __ROL4__(digest_permu[9], 19) ^ __ROL4__(digest_permu[9], 30))
-                     + v7;
-
-      v8 = work_buffer[res_item_ctr + 2]
-         + dword_4054F4[res_item_ctr + 2]
-         + (digest_permu[12] ^ digest_permu[14] & (digest_permu[12] ^ digest_permu[13]))
-         + digest_permu[11]
-         + (__ROL4__(digest_permu[14], 7) ^ __ROL4__(digest_permu[14], 21) ^ __ROL4__(digest_permu[14], 26));
-
-      digest_permu[15] += v8;
-
-      digest_permu[11] = (digest_permu[9] & digest_permu[10] | dgr_val0 & (digest_permu[9] | digest_permu[10]))
-                     + (__ROL4__(digest_permu[10], 10) ^ __ROL4__(digest_permu[10], 19) ^ __ROL4__(digest_permu[10], 30))
-                     + v8;
-
-      v9 = work_buffer[res_item_ctr + 3]
-         + dword_4054F4[res_item_ctr + 3]
-         + (digest_permu[13] ^ digest_permu[15] & (digest_permu[13] ^ digest_permu[14]))
-         + digest_permu[12]
-         + (__ROL4__(digest_permu[15], 7) ^ __ROL4__(digest_permu[15], 21) ^ __ROL4__(digest_permu[15], 26));
-
-      v10 = v9 + dgr_val0;
-
-      digest_permu[12] = (digest_permu[10] & digest_permu[11] | digest_permu[9] & (digest_permu[10] | digest_permu[11]))
-                     + (__ROL4__(digest_permu[11], 10) ^ __ROL4__(digest_permu[11], 19) ^ __ROL4__(digest_permu[11], 30))
-                     + v9;
-
-      v11 = work_buffer[res_item_ctr + 4]
-         + dword_4054F4[res_item_ctr + 4]
-         + (digest_permu[14] ^ v10 & (digest_permu[14] ^ digest_permu[15]))
-         + digest_permu[13]
-         + (__ROL4__(v10, 7) ^ __ROL4__(v10, 21) ^ __ROL4__(v10, 26));
-      digest_permu[9] += v11;
-
-      digest_permu[13] = (digest_permu[11] & digest_permu[12] | digest_permu[10] & (digest_permu[11] | digest_permu[12]))
-                     + (__ROL4__(digest_permu[12], 10) ^ __ROL4__(digest_permu[12], 19) ^ __ROL4__(digest_permu[12], 30))
-                     + v11;
-
-      v12 = work_buffer[res_item_ctr + 5]
-         + dword_4054F4[res_item_ctr + 5]
-         + (digest_permu[15] ^ digest_permu[9] & (v10 ^ digest_permu[15]))
-         + digest_permu[14]
-         + (__ROL4__(digest_permu[9], 7) ^ __ROL4__(digest_permu[9], 21) ^ __ROL4__(digest_permu[9], 26));
-      digest_permu[10] += v12;
-
-      digest_permu[14] = (digest_permu[12] & digest_permu[13] | digest_permu[11] & (digest_permu[12] | digest_permu[13]))
-                     + (__ROL4__(digest_permu[13], 10) ^ __ROL4__(digest_permu[13], 19) ^ __ROL4__(digest_permu[13], 30))
-                     + v12;
-
-      v13 = work_buffer[res_item_ctr + 6]
-         + dword_4054F4[res_item_ctr + 6]
-         + (v10 ^ digest_permu[10] & (v10 ^ digest_permu[9]))
-         + digest_permu[15]
-         + (__ROL4__(digest_permu[10], 7) ^ __ROL4__(digest_permu[10], 21) ^ __ROL4__(digest_permu[10], 26));
-      digest_permu[11] += v13;
-
-      digest_permu[15] = (digest_permu[13] & digest_permu[14] | digest_permu[12] & (digest_permu[13] | digest_permu[14]))
-                     + (__ROL4__(digest_permu[14], 10) ^ __ROL4__(digest_permu[14], 19) ^ __ROL4__(digest_permu[14], 30))
-                     + v13;
-
-      v14 = work_buffer[res_item_ctr + 7]
-         + dword_4054F4[res_item_ctr + 7]
-         + (digest_permu[9] ^ digest_permu[11] & (digest_permu[9] ^ digest_permu[10]))
-         + (__ROL4__(digest_permu[11], 7) ^ __ROL4__(digest_permu[11], 21) ^ __ROL4__(digest_permu[11], 26))
-         + v10;
-
-      res_item_ctr += 8;
-
-      digest_permu[12] += v14;
-
-      dgr_val0 = (digest_permu[14] & digest_permu[15] | digest_permu[13] & (digest_permu[14] | digest_permu[15]))
-               + (__ROL4__(digest_permu[15], 10) ^ __ROL4__(digest_permu[15], 19) ^ __ROL4__(digest_permu[15], 30))
-               + v14;
-   }
-   while ( res_item_ctr <= 0x3F );
 
    res_acc0 = digest_permu[15];
    *digest2_result = dgr_val0 + digest_permu[5];
