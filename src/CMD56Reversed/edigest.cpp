b78f9d3f7cc889a9688e0c910e9274add92906ac
motoharu-gosuto <motoharu.gosuto@gmail.com>
2018-07-15 19:21:35 +0400

    reverse edgest

--- src/CMD56Reversed/edigest.cpp
+++ src/CMD56Reversed/edigest.cpp
@@ -30,19 +30,30 @@ const int dword_4054F4[64] =
 //change digest_permu[13]
 void do_block0(const int* work_buffer, int* digest_permu, const int res_item_ctr)
 {
-   int cycle_acc;
+   int val0 = digest_permu[9];
+   int val1 = digest_permu[10];
+   int val2 = digest_permu[11];
+   int val3 = digest_permu[12];
 
-   cycle_acc = work_buffer[res_item_ctr]
-                  + dword_4054F4[res_item_ctr]
-                  + (digest_permu[10] ^ digest_permu[12] & (digest_permu[10] ^ digest_permu[11]))
-                  + digest_permu[9]
-                  + (__ROL4__(digest_permu[12], 7) ^ __ROL4__(digest_permu[12], 21) ^ __ROL4__(digest_permu[12], 26));
+   int term0 = work_buffer[res_item_ctr];
+   int term1 = dword_4054F4[res_item_ctr];
+   int term2 = val0;
+   int term3 = (val1 ^ val3 & (val1 ^ val2));
+   int term4 = (__ROL4__(val3, 7) ^ __ROL4__(val3, 21) ^ __ROL4__(val3, 26));
 
-   digest_permu[13] += cycle_acc;
+   int acc0 = term0 + term1 + term2 + term3 + term4;
 
-   digest_permu[9] = (digest_permu[0] & digest_permu[15] | digest_permu[14] & (digest_permu[0] | digest_permu[15]))
-                  + (__ROL4__(digest_permu[0], 10) ^ __ROL4__(digest_permu[0], 19) ^ __ROL4__(digest_permu[0], 30))
-                  + cycle_acc;
+   digest_permu[13] += acc0;
+
+   int val4 = digest_permu[0];
+   int val5 = digest_permu[14];
+   int val6 = digest_permu[15];
+
+   int term5 = (val4 & val6 | val5 & (val4 | val6));
+   int term6 = (__ROL4__(val4, 10) ^ __ROL4__(val4, 19) ^ __ROL4__(val4, 30));
+   int term7 = acc0;
+
+   digest_permu[9] = term5 + term6 + term7;
 }
 
 //change digest_permu[10]
