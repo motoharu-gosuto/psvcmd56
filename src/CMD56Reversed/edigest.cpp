090c3c51c57dc160ae370b7a13cb34fa31eb3fc4
motoharu-gosuto <motoharu.gosuto@gmail.com>
2018-07-15 23:00:09 +0400

    simplify do_block5

--- src/CMD56Reversed/edigest.cpp
+++ src/CMD56Reversed/edigest.cpp
@@ -140,19 +140,34 @@ void do_block4(const unsigned int* work_buffer, unsigned int* digest_permu, cons
 //change digest_permu[14]
 void do_block5(const unsigned int* work_buffer, unsigned int* digest_permu, const unsigned int res_item_ctr, const unsigned int carry)
 {
-   int v12;
+   int val30 = work_buffer[res_item_ctr + 5];
+   int val31 = dword_4054F4[res_item_ctr + 5];
+   int val0 = digest_permu[14];
+   
+   int val1 = digest_permu[15];
+   int val3 = digest_permu[9];
+   int val2 = carry;
 
-   v12 = work_buffer[res_item_ctr + 5]
-            + dword_4054F4[res_item_ctr + 5]
-            + (digest_permu[15] ^ digest_permu[9] & (carry ^ digest_permu[15]))
-            + digest_permu[14]
-            + (__ROL4__(digest_permu[9], 7) ^ __ROL4__(digest_permu[9], 21) ^ __ROL4__(digest_permu[9], 26));
+   int term0 = val30;
+   int term1 = val31;
+   int term2 = val0;
+   int term3 = val1 ^ (val3 & (val1 ^ val2));
+   int term4 = __ROL4__(val3, 7) ^ __ROL4__(val3, 21) ^ __ROL4__(val3, 26);
 
-   digest_permu[10] += v12;
+   int acc0 = term0 + term1 + term2 + term3 + term4;
 
-   digest_permu[14] = (digest_permu[12] & digest_permu[13] | digest_permu[11] & (digest_permu[12] | digest_permu[13]))
-                  + (__ROL4__(digest_permu[13], 10) ^ __ROL4__(digest_permu[13], 19) ^ __ROL4__(digest_permu[13], 30))
-                  + v12;
+   int val4 = digest_permu[13];
+   int val5 = digest_permu[11];
+   int val6 = digest_permu[12];
+   int val7 = digest_permu[10];
+   
+   int term5 = (val4 & val6) | (val5 & (val4 | val6));
+   int term6 = __ROL4__(val4, 10) ^ __ROL4__(val4, 19) ^ __ROL4__(val4, 30);
+   int term7 = acc0;
+
+   digest_permu[10] = val7 + acc0;
+
+   digest_permu[14] = term5 + term6 + term7;
 }
 
 //change digest_permu[11]
