3a6124a8b321d5b515e6e603d9126955455ada78
motoharu-gosuto <motoharu.gosuto@gmail.com>
2018-10-07 15:08:33 +0400

    refactor and extend internal sched proxy types

--- src/CMD56Reversed/SceSblSmSchedProxyInternalTypes.h
+++ src/CMD56Reversed/SceSblSmSchedProxyInternalTypes.h
@@ -4,6 +4,12 @@
 
 #define INVALID_SM_OPERATION_HANDLE -1
 
+#define INVALID_SM_OPERATION_ID -1
+
+#define SM_PACK_HANDLE(ptr) (((unsigned int)ptr >> 1) | 1)
+
+#define SM_UNPACK_HANDLE(type, ptr) ((type*)(((unsigned int)ptr >> 1) << 2))
+
 typedef unsigned int SmOperationHandle;
 
 struct item_996E0C //size is 0x10
@@ -33,6 +39,7 @@ struct sched_proxy_state_t
   int state;
 };
 
+//resembles smc_12D_core_ctx_intr_entry
 struct operation_callback_data
 {
   int func_arg2;
@@ -40,7 +47,7 @@ struct operation_callback_data
   int func_arg3;
 };
 
-struct shed_proxy_operation_callback_entry_t
+struct proxy_intr_entry_t
 {
    smc_138_callback *cb;
    operation_callback_data data;
@@ -53,7 +60,7 @@ struct shed_proxy_operation_item_t
   SmOperationHandle operation_handle;
   SceUID SceSblSmsProxy_event_uid;
   SceUID SceSblSmsProxyWait_mutex_uid;
-  shed_proxy_operation_callback_entry_t functions[4];
+  proxy_intr_entry_t intr_entries[4];
 };
 
 struct SceSblSmschedProxyHeap
@@ -61,6 +68,7 @@ struct SceSblSmschedProxyHeap
   shed_proxy_operation_item_t items[0x40];
 };
 
+//size is 0x30
 struct smc_12D_data_self_t
 {
   unsigned int self_type;
@@ -69,45 +77,43 @@ struct smc_12D_data_self_t
   uint8_t caller_capability[32];
 };
 
+//size is 0x48u
 struct smc_12D_data_t
 {
-   int unk0;
-   int unk4;
-   int unk8;
-   int unkC;
+   int unk0; //unknown input parameter, copied into operation handle
+   int unk4; //unknown input parameter, copied into operation handle
+   int unk8; //unknown input parameter, copied into operation handle
+   int unkC; //unknown input parameter, copied into operation handle
    SmOperationId invoke_operation_id;
    SmOperationHandle operation_handle;
    smc_12D_data_self_t caller_self_info;
 };
 
-struct smc_12E_data_t
-{
-  int unk0;
-  int unk4;
-};
-
-struct smc_12F_data_t
-{
-  int status0;
-  int status1;
-};
-
 struct smc_134_137_data_t
 {
-  int unk0;
+  int f00d_fifo_register_value;
+};
+
+struct smc_134_data_t
+{
+  int f00d_fifo_register_value;
+};
+
+struct smc_137_data_t
+{
+  int f00d_fifo_register_value;
 };
 
 #define INTR_SET_EVENT_FLAG 1
 #define INTR_CALL_CB_EX 2
 #define INTR_CALL_CB 4
 
+// size is 0x1C
 struct smc_13A_intr_data_t
 {
-  int function_index;
+  int intr_index;
   SmOperationId operation_id;
   int cb_call_mode_flags;
   int event_flag_bits;
-  int smc_arg2;
-  int func_arg4;
-  int func_arg3;
+  operation_callback_data data;
 };
