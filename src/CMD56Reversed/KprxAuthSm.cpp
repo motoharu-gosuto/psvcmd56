4620ea46684aa01703d4081ea1ec12aaab90aeb7
motoharu-gosuto <motoharu.gosuto@gmail.com>
2019-01-03 18:54:21 +0300

    simplify handle_command_0x50001_0x60001_80E568

--- src/CMD56Reversed/KprxAuthSm.cpp
+++ src/CMD56Reversed/KprxAuthSm.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
 
+#include "F00D/IF00DService.h"
+#include "SceSblSmSchedProxyTypes.h"
+
 int cookie_810928 = 0;
 
 char key0_81277C[1];
@@ -43,183 +46,113 @@ int sub_80E4CC(char*, int, char*, char*)
    return 0;
 }
 
-int handle_command_0x50001_0x60001_80E568(int arg1, char* request_buffer)
+int handle_command_0x50001_0x60001_80E568(int arg1, SceSblSmschedCallFuncCommand* request_buffer)
 {
-   int buffer_copy;
-   int command_168;
-   int return_value_164 = 0;
-   int unused_160;
-   int var_10_15C;
-   int var_14_158;
-   int var_18_154;
-   int var_1C_150;
-   int var_20_14C;
-   int var_24_148;
-   int var_28_144;
-   int var_2C_140;
-   int var_30_13C;
-   int var_34_138;
-   int var_38_134;
-   int var_3C_130;
-   int key_12C;
-   int key_128;
-   int key_124;
-   int key_120;
-   int key_11C;
-   int key_118;
-   int key_114;
-   int key_110;
+   SceSblSmschedCallFuncCommand raw_buffer;
 
-   int key_size_2C;
-   int key_id_28;
-   int slot_id_24;
-   int var_20;
+   memcpy((char*)&raw_buffer, (char*)request_buffer, 0x150);
 
-   char* key_dest;
-   
-   int reg1 = buffer_copy; //add3    $1, $sp, 0x24
+   SceSblSmCommKprxAuthData_50001* request;
+   request = (SceSblSmCommKprxAuthData_50001*)raw_buffer.data;
 
-   int reg3 = 0x150;
-   char* reg5 = request_buffer;
+   char key_dest[0x20];
 
-   memcpy((char*)&buffer_copy, request_buffer, 0x150);
+   //check that size is aligned
 
-   int reg3 = key_size_2C;
-   int reg2 = reg3 & 0xF;
-
-   if(reg2 != 0)
+   if((request->key_size & 0xF) != 0)
    {
-      handle_error_80E68E:
-
-      #pragma region handle_error_80E68E
-
-      reg3 = reg3 | 0x800F0000;
-      reg3 = reg3 | 0x16;
-      
-      return_value_164 = reg3;
-      
-      //int reg2 = cookie_1C;
-      //int reg3 = cookie_810928;
-
-      //if (reg2 != reg3)
-      //{
-      //   sub_80B092();
-      //}
-
-      //add3    $sp, $sp, 0x178
-   
-      //reg6 = prev_state_10;
-      //reg5 = prev_state_C;
-      //reg11 = prev_state_14;
-
-      //add     $sp, 0x18
-      //jmp     $11
-
-      #pragma endregion
+      raw_buffer.response = 0x800F0016;
+      return -1;
    }
 
-   if(command_168 == 0x50001)
+   //check slot id
+
+   if(request->slot_id < 0xC || request->slot_id > 0x17)
    {
-      if(key_size_2C == 0)
-      {
-         goto handle_error_80E68E;
-      }
+      raw_buffer.response = 0x800F0016;
+      return -1;
    }
 
-   switch(key_id_28)
-   {
-   case 0x00000000:
-   case 0x00010000:
-   case 0x00010001:
-   case 0x00020000:
-   case 0x00020001:
-      break;
-   default:
-      {
-         goto handle_error_80E68E;
-      }
-   }
-
-   if(slot_id_24 < 0xC || slot_id_24 > 0x17)
-   {
-      goto handle_error_80E68E;
-   }
-
-   if(key_id_28 == 0x00010001)
-   {
-      int reg0 = sub_80D318();
-      
-      if(reg0 == 0)
-      {
-         goto handle_error_80E68E;
-      }
-   }
+   // do some scrambling - result lost after memcpy ?
 
    int reg0 = sub_8102BC(key_dest, 0, 0x20);
 
-   if(command_168 == 0x50001)
+   //execute command
+
+   if(raw_buffer.service_id == 0x50001)
    {
-      if(key_size_2C > 0x20)
+      if(request->key_size <= 0 || request->key_size > 0x20)
       {
-         goto handle_error_80E68E;
+         raw_buffer.response = 0x800F0016;
+         return -1;
       }
+      
+      memcpy(key_dest, request->key, request->key_size);
 
-      memcpy(key_dest, (char*)&key_12C, key_size_2C);
+      //execute command according to specific key id
 
-      switch(key_id_28)
+      switch(request->key_id)
       {
       case 0x00000000:
          {
-            return_value_164 = sub_80E162(key_dest, key_size_2C, key0_81277C);
-            if(return_value_164 == 0)
-               return_value_164 = set_dmac5_key_80E26A(key_dest, key_size_2C / 4, slot_id_24);
+            raw_buffer.response = sub_80E162(key_dest, request->key_size, key0_81277C);
+            if(raw_buffer.response == 0)
+               raw_buffer.response = set_dmac5_key_80E26A(key_dest, request->key_size / 4, request->slot_id);
          }
          break;
       case 0x00010000:
          {
-            return_value_164 = sub_80E162(key_dest, key_size_2C, key1_81279C);
-            if(return_value_164 == 0)
-               return_value_164 = set_dmac5_key_80E26A(key_dest, key_size_2C / 4, slot_id_24);
+            raw_buffer.response = sub_80E162(key_dest, request->key_size, key1_81279C);
+            if(raw_buffer.response == 0)
+               raw_buffer.response = set_dmac5_key_80E26A(key_dest, request->key_size / 4, request->slot_id);
          }
          break;
       case 0x00010001:
          {
-            return_value_164 = sub_80E162(key_dest, key_size_2C, key2_8127BC);
-            if(return_value_164 == 0)
-               return_value_164 = set_dmac5_key_80E26A(key_dest, key_size_2C / 4, slot_id_24);
+            int reg0 = sub_80D318(); // some dmac setup
+            if(reg0 == 0)
+            {
+               raw_buffer.response = 0x800F0016;
+               return -1;
+            }
+
+            raw_buffer.response = sub_80E162(key_dest, request->key_size, key2_8127BC);
+            if(raw_buffer.response == 0)
+               raw_buffer.response = set_dmac5_key_80E26A(key_dest, request->key_size / 4, request->slot_id);
          }
          break;
       case 0x00020000:
          {
-            return_value_164 = sub_80E43E(key_dest, key_size_2C, key3_8127FC, key3_8127FC + 0x10);
-            if(return_value_164 == 0)
-               return_value_164 = set_dmac5_key_80E26A(key_dest, key_size_2C / 4, slot_id_24);
+            raw_buffer.response = sub_80E43E(key_dest, request->key_size, key3_8127FC, key3_8127FC + 0x10);
+            if(raw_buffer.response == 0)
+               raw_buffer.response = set_dmac5_key_80E26A(key_dest, request->key_size / 4, request->slot_id);
          }
          break;
       case 0x00020001:
          {
-            return_value_164 = sub_80E4CC(key_dest, key_size_2C, key4_81281C, key4_81281C + 0x20);
-            if(return_value_164 == 0)
-               return_value_164 = set_dmac5_key_80E26A(key_dest, key_size_2C / 4, slot_id_24);
+            raw_buffer.response = sub_80E4CC(key_dest, request->key_size, key4_81281C, key4_81281C + 0x20);
+            if(raw_buffer.response == 0)
+               raw_buffer.response = set_dmac5_key_80E26A(key_dest, request->key_size / 4, request->slot_id);
          }
          break;
       default:
          {
-            if(return_value_164 == 0) //uses the value that was supplied in memcpy
-               return_value_164 = set_dmac5_key_80E26A(key_dest, key_size_2C / 4, slot_id_24);
+            raw_buffer.response = 0x800F0016;
+            return -1;
          }
          break;
       }
    }
-   else if(command_168 == 0x60001)
+   else if(raw_buffer.service_id == 0x60001)
    {
       memset(key_dest, 0, 0x20);
 
-      return_value_164 = set_dmac5_key_80E26A(key_dest, 0x20 / 4, slot_id_24);
+      raw_buffer.response = set_dmac5_key_80E26A(key_dest, 0x20 / 4, request->slot_id);
    }
    else
    {
-      goto handle_error_80E68E;
+      raw_buffer.response = 0x800F0016;
+      return -1;
    }
 
    return 0;
